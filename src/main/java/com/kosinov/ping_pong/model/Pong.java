0355ba29ec6ca1eef66490aac4d8bda0031d1a19
kosinovv <kosinov_va@mail.ru>
2024-07-31 19:30:58 +0300

    Доработка описания README.md Реализован сервис PONG, который после приема ping сообщения, формирует ответ на принятое сообщение. Реализован прием сервисом PING ответного сообщения от сервиса PONG.

--- src/main/java/com/kosinov/ping_pong/model/Pong.java
+++ src/main/java/com/kosinov/ping_pong/model/Pong.java
@@ -1,11 +1,54 @@
 package com.kosinov.ping_pong.model;
 
-import lombok.Data;
+import java.time.LocalDateTime;
+import java.util.Objects;
 
-import java.util.Date;
-
-@Data
 public class Pong {
-    public final int pong_id;
-    public final Date pong_date_time;
+    private int pong_id;
+    private LocalDateTime pong_date_time;
+
+    public Pong() {
+    }
+
+    public Pong(int pong_id, LocalDateTime pong_date_time) {
+        this.pong_id = pong_id;
+        this.pong_date_time = pong_date_time;
+    }
+
+    public int getPong_id() {
+        return pong_id;
+    }
+
+    public void setPong_id(int pong_id) {
+        this.pong_id = pong_id;
+    }
+
+    public LocalDateTime getPong_date_time() {
+        return pong_date_time;
+    }
+
+    public void setPong_date_time(LocalDateTime pong_date_time) {
+        this.pong_date_time = pong_date_time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Pong pong = (Pong) o;
+        return Objects.equals(pong_id, pong.pong_id) && Objects.equals(pong_date_time, pong.pong_date_time);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pong_id, pong_date_time);
+    }
+
+    @Override
+    public String toString() {
+        return "Pong{" +
+                "pong_id=" + pong_id +
+                ", pong_date_time=" + pong_date_time +
+                '}';
+    }
 }
--- src/main/java/com/kosinov/ping_pong/services/PingService.java
+++ src/main/java/com/kosinov/ping_pong/services/PingService.java
@@ -2,11 +2,14 @@ package com.kosinov.ping_pong.services;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.kosinov.ping_pong.model.Ping;
+import com.kosinov.ping_pong.model.Pong;
 import com.kosinov.ping_pong.queues.PingProducer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.function.Consumer;
 
 
 @Service
@@ -25,5 +28,12 @@ public class PingService {
         pingCount = pingCount +1;
         Ping ping = new Ping(pingCount, LocalDateTime.now());
         pingProducer.sendPing(ping);
+    }
+
+    @Bean
+    public Consumer<Pong> pongMsgConsumer() {
+        return msg -> {
+            System.out.printf("Pong_id %s, Pong_date_time: %s\n", msg.getPong_id(), msg.getPong_date_time());
+        };
     }
 }
